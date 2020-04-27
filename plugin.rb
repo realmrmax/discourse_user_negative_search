214c991caa0e85b751624ba29368303f7186131d
Max <realmrmax@gmail.com>
2020-04-27 11:26:30 +0300

    'update'

--- plugin.rb
+++ plugin.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
-# name: discourse-knowledge-explorer
-# about: A plugin to make it easy to explore and find knowledge base-type articles in Discourse
-# version: 0.1
-# author: Justin DiRose
-
-enabled_site_setting :knowledge_explorer_enabled
-
-register_asset 'stylesheets/common/knowledge-explorer.scss'
-register_asset 'stylesheets/mobile/knowledge-explorer.scss'
-
-load File.expand_path('lib/knowledge_explorer/engine.rb', __dir__)
-load File.expand_path('lib/knowledge_explorer/query.rb', __dir__)
 
 after_initialize do
   require_dependency 'search'
