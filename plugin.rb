8e1217a0eef40cefe1d429a386bbeafead9251c4
Max <realmrmax@gmail.com>
2020-04-27 12:31:57 +0300

    'categories'

--- plugin.rb
+++ plugin.rb
@@ -16,5 +16,30 @@ after_initialize do
       		posts.where("1 = 0")
     	end      
      end
+
+  Search.advanced_filter(/^\-category:(.+)$/) do |posts, match|
+    exact = false
+
+    if match[0] == "="
+      exact = true
+      match = match[1..-1]
+    end
+
+    category_ids = Category.where('slug ilike ? OR name ilike ? OR id = ?',
+                                  match, match, match.to_i).pluck(:id)
+    if category_ids.present?
+
+      unless exact
+        category_ids +=
+          Category.where('parent_category_id = ?', category_ids.first).pluck(:id)
+      end
+
+      @category_filter_matched ||= true
+      posts.where("topics.category_id NOT IN (?)", category_ids)
+    else
+      posts.where("1 = 0")
+    end
+  end
+
    end
 end
